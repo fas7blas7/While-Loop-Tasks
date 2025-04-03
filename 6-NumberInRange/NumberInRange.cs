5feeb9bcbf25e5e4462373145e2ed6be1b1b8068
Martin Kirov <skylinecho@abv.bg>
2025-04-03 20:51:27 +0300

    Adding NumberInRange task

---
+++ 6-NumberInRange/NumberInRange.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.Design;
+
+int number = int.Parse(Console.ReadLine());
+
+while (number < 1 || number > 100)
+{
+    number = int.Parse(Console.ReadLine());
+}
+    
+Console.WriteLine($"{number}");

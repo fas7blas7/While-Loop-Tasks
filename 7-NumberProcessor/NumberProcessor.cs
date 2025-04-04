1729eb9e44c3200f0e4e317f1e1769d0ee0f73c1
Martin Kirov <skylinecho@abv.bg>
2025-04-04 21:24:48 +0300

    Adding NumberProcessor task

---
+++ 7-NumberProcessor/NumberProcessor.cs
@@ -0,0 +1,18 @@
+﻿int initialNumber = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+while (command != "End")
+{
+    switch (command)
+    {
+        case "Inc":
+            initialNumber++;
+            break;
+        case "Dec":
+            initialNumber--;
+            break;        
+    }    
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(initialNumber);
